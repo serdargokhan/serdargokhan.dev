9dfd9600456c04815792ab75c5ce74ff989ff6f7
serdargokhan <gokhan_bakirci_53@hotmail.com>
2022-04-30 16:26:53 +0300

    small changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,16 +12,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="true"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap"
-                    rel="stylesheet"
-                />
                 <link
                     rel="shortcut icon"
                     type="image/x-icon"
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html className="scroll-smooth" lang="en" dir="ltr">
-            <Head />
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="true"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
             <body>
                 <Main />
                 <NextScript />

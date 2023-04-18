86f0a9183dd1602de56e251290ab615681393f27
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-04-18 16:13:04 +0300

    feat: add next-link to the logo in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
-import LogoG from "public/Navbar/LogoG.svg";
+import Link from "next/link";
+import SiteLogo from "public/site-logo.svg";
 import NavLinks from "./NavLinks";
 import { NextImage } from "../common";
 
@@ -6,15 +7,15 @@ function Navbar() {
     return (
         <nav className="h-16 bg-primary-100">
             <div className="mx-auto w-11/12 max-w-7xl pt-6 sm:flex sm:items-center sm:justify-between">
-                <div className="hidden transition-all duration-300 hover:-translate-y-1 hover:cursor-pointer sm:flex">
+                <Link href="/" className="hidden sm:inline-block">
                     <NextImage
                         width={30}
                         height={30}
-                        src={LogoG}
+                        src={SiteLogo}
                         alt=""
                         priority
                     />
-                </div>
+                </Link>
                 <NavLinks />
             </div>
         </nav>

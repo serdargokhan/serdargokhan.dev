28f9011232b6ab5a457f405c5a800acc66aab817
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-04-18 13:54:26 +0300

    fix: update tw config due to structure change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {

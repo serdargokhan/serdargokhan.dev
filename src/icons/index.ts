57a02608ccbb0dedafdca7ecd384459f808d0ad7
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-04-18 13:57:11 +0300

    feat: make icons reusable components

---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as ReactIcon } from "./React";
+export { default as JavascriptIcon } from "./Javascript";
+export { default as NextIcon } from "./Next";
+export { default as GithubIcon } from "./Github";
+export { default as ExternalLinkIcon } from "./ExternalLink";

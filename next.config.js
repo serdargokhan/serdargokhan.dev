32e94474ff6e201df2323af185057b4688e9a0ed
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-05-04 23:51:48 +0300

    feat: remove experimental appDir due to stable version

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const withNextIntl = require('next-intl/plugin')(
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
   headers() {
     return [
       {

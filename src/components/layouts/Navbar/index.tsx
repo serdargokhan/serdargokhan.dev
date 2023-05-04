fcaf4f820ac252daac8b61b4b118a2a063b9e237
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-05-04 23:53:29 +0300

    fix: navigate to the top with animation scroll on icon click

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,24 +1,23 @@
-import Link from "next/link";
+import { NextImage } from "@src/components/common";
 import SiteLogo from "@public/images/common/site-logo.svg";
 import NavLinks from "./NavLinks";
-import { NextImage } from "../../common";
 
 function Navbar() {
     return (
         <nav className="h-16 bg-primary-300 flex items-center sticky top-0 border-b-2 border-primary-100 z-50">
             <div className="container flex items-center justify-between">
-                <Link
-                    href="/"
+                <a
+                    href="#"
                     className="bg-primary-100 p-1 rounded-full hover:shadow-primary-400 transition"
                 >
                     <NextImage
                         width={28}
                         height={28}
                         src={SiteLogo}
-                        alt=""
                         priority
+                        alt=""
                     />
-                </Link>
+                </a>
                 <NavLinks />
             </div>
         </nav>

c9b1887d1eabd4afaffdc72930abdc40f43fd6fd
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-05-06 16:25:12 +0300

    feat: implement locale-switcher

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { Label } from "@src/components/ui/Label";
+import { Switch } from "@src/components/ui/Switch";
+import { useRouter, usePathname } from "next/navigation";
+
 function Footer() {
+    const router = useRouter();
+
+    const pathname = usePathname();
+
     return (
         <footer className="h-16 flex items-center bg-primary-300 text-primary-400 cursor-default">
-            <div className="container flex justify-between text-sm">
+            <div className="container flex justify-between text-sm items-center">
                 <p>Made with ❤️ in Istanbul © {new Date().getFullYear()}</p>
-                <p>Serdar Gökhan</p>
+                <div className="flex items-center space-x-2 font-semibold">
+                    <Label htmlFor="lang">TR</Label>
+                    <Switch
+                        id="lang"
+                        className="bg-primary-100 border-white [&>span]:bg-primary-300"
+                        defaultChecked={pathname !== "/tr"}
+                        onCheckedChange={checkedValue => {
+                            router.push(checkedValue ? "/" : "/tr");
+                        }}
+                    />
+                    <Label htmlFor="lang">EN</Label>
+                </div>
             </div>
         </footer>
     );

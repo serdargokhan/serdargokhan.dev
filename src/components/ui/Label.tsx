5e49bb893e77d25ebc67bf4d7df0373b3e6b365d
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-05-06 16:24:42 +0300

    feat: implement reusable ui components

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cn } from "@src/utils";
+
+const Label = React.forwardRef<
+    React.ElementRef<typeof LabelPrimitive.Root>,
+    React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+>(({ className, ...props }, ref) => (
+    <LabelPrimitive.Root ref={ref} className={cn(className)} {...props} />
+));
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };

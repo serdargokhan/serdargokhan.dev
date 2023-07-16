9b478ba88be3f9477fdac649b8f1e773eed15990
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-07-16 19:31:51 +0300

    fix: sitemap lastModified property

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: "https://serdargokhan.dev",
-            lastModified: new Date()
+            lastModified: new Date().toISOString().split("T")[0]
         }
     ];
 }

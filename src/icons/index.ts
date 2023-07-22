3e9017c0ee950de0fac415a5c4b119fd305b7d0a
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-07-22 18:24:39 +0300

    feat: add linkedin and twitter icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,3 +3,5 @@ export { default as JavascriptIcon } from "./Javascript";
 export { default as NextIcon } from "./Next";
 export { default as GithubIcon } from "./Github";
 export { default as ExternalLinkIcon } from "./ExternalLink";
+export { default as LinkedinIcon } from "./Linkedin";
+export { default as TwitterIcon } from "./Twitter";

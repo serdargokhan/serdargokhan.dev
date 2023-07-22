b33c62a8a0c4bb137b9353fc9880a0b1fba1ccd7
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-07-22 18:25:17 +0300

    feat: add social media links and improvements for footer

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -3,6 +3,7 @@
 import { useRouter, usePathname } from "next/navigation";
 import { Label } from "@src/components/ui/Label";
 import { Switch } from "@src/components/ui/Switch";
+import { GithubIcon, LinkedinIcon, TwitterIcon } from "@src/icons";
 
 function Footer() {
     const router = useRouter();
@@ -11,9 +12,34 @@ function Footer() {
     return (
         <footer className="h-footer flex cursor-default items-center bg-primary-300 text-primary-400">
             <div className="container flex items-center justify-between text-sm">
-                <p>Made with ❤️ © {new Date().getFullYear()}</p>
+                <div className="flex items-center gap-2">
+                    <a
+                        href="https://github.com/serdargokhan"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        <GithubIcon className="h-8 w-8 fill-white transition hover:fill-primary-100" />
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/serdarrgokhann"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        <LinkedinIcon className="h-8 w-8 fill-white transition hover:fill-primary-100" />
+                    </a>
+                    <a
+                        href="https://twitter.com/serdarrgokhann"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        <TwitterIcon className="h-8 w-8 fill-white transition hover:fill-primary-100" />
+                    </a>
+                </div>
+                <p className="hidden sm:inline-block">
+                    Made with ❤️ © {new Date().getFullYear()}
+                </p>
                 <div className="flex items-center space-x-2 font-semibold">
-                    <Label htmlFor="lang">TR</Label>
+                    <Label htmlFor="lang">Türkçe</Label>
                     <Switch
                         id="lang"
                         aria-label="Locale Switcher"
@@ -23,7 +49,7 @@ function Footer() {
                             router.push(checkedValue ? "/" : "/tr");
                         }}
                     />
-                    <Label htmlFor="lang">EN</Label>
+                    <Label htmlFor="lang">English</Label>
                 </div>
             </div>
         </footer>

466f86dc67f06b98552db568f9cf025d3519b6a0
Serdar Gökhan BAKIRCI <sgbakirci@borusan.com>
2026-09-18 23:08:45 +0300

    fix: update email address in contact section

--- src/components/pages/home/contact-section/index.tsx
+++ src/components/pages/home/contact-section/index.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from "next-intl";
 import ExternalLink from "@src/components/common/external-link";
 import CopyEmailButton from "./copy-email-button";
 
-const email = "gokhanbakirci5353@gmail.com";
+const email = "me@serdargokhan.dev";
 
 export default function ContactSection() {
     const t = useTranslations("Home.ContactSection");
